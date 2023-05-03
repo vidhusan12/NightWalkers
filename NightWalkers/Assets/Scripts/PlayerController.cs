d02378a3ebd7cdf550bca7c9fdb65ad5c1429006
vidhusan12 <vidhusan709@gmail.com>
2023-05-04 00:03:23 +1200

    Player Movement
    Made a basic player movement

--- NightWalkers/Assets/Scripts/PlayerController.cs
+++ NightWalkers/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //fixed movement speed
+    public float moveSpeed;
+    private Rigidbody myRigidbody;
+    private Vector3 moveInput;
+    private Vector3 moveVelocity;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
+        myRigidbody = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"),0f,Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
         
+    }
+
+    void FixedUpdate(){
+        myRigidbody.velocity = moveVelocity;
     }
 }
