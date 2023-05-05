905c35bdf3d134d912956367d58870a55acf5c01
Dev <xtreamgamer24@gmail.com>
2023-05-05 17:27:07 +1200

    Updated HealthBar
    I decided to add the health bar and shift it on the top left.

--- NightWalkers/Assets/Scripts/PlayersHealth.cs
+++ NightWalkers/Assets/Scripts/PlayersHealth.cs
@@ -29,7 +29,7 @@ public class PlayersHealth : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            TakeDamage(20);
+            TakeDamage(15);
         }
     }
 }
